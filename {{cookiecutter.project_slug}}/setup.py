18c06b7d18f497d2d36340bafcd67ea45f607de8
Daniel Scheffler <danschef@gfz-potsdam.de>
2023-06-09 17:14:32 +0200

    Drop Python<3.8 support and add support for Python<=3.11.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -161,7 +161,7 @@ req_dev = ['twine'] + req_setup + req_test + req_doc + req_lint
 setup(
     author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
     author_email='{{ cookiecutter.email }}',
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -170,10 +170,10 @@ setup(
 {%- endif %}
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
     ],
     description="{{ cookiecutter.project_short_description }}",
     {%- if 'no' not in cookiecutter.command_line_interface|lower %}
